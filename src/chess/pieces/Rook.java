cdd3df4be283cfeb59f6f682c175b1a7251d4573
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-11-24 10:05:07 -0300

    Implementing possible moves of King

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -23,7 +23,7 @@ public class Rook extends ChessPiece {
 		Position p = new Position(0, 0);
 
 		// above
-		p.setValuew(position.getRow() - 1, position.getColumn());
+		p.setValues(position.getRow() - 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() - 1);
@@ -33,7 +33,7 @@ public class Rook extends ChessPiece {
 		}
 
 		// left
-		p.setValuew(position.getRow(), position.getColumn() - 1);
+		p.setValues(position.getRow(), position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() - 1);
@@ -44,7 +44,7 @@ public class Rook extends ChessPiece {
 		}
 
 		// right
-		p.setValuew(position.getRow(), position.getColumn() + 1);
+		p.setValues(position.getRow(), position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() + 1);
@@ -55,7 +55,7 @@ public class Rook extends ChessPiece {
 		}
 
 		// below
-		p.setValuew(position.getRow() + 1, position.getColumn());
+		p.setValues(position.getRow() + 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() + 1);
