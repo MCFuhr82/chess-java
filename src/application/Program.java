28ab2141a77f4152892de8c9bf927f58b214a085
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-11-25 10:30:27 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Digte uma peca para promocao: (B/C/T/D)");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e ) {
 				System.out.println(e.getMessage());
